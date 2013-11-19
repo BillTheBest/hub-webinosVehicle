7b87f7375603612cb044dafe329172019f87b29b
Christos Botsikas <cbot@epu.ntua.gr>
2013-11-20 01:14:43 +0200

    Fix labels and default values
    Jira: HUBWV-20

--- autoview/obd-manager/tripcomputer_obdreview/cbcc.js
+++ autoview/obd-manager/tripcomputer_obdreview/cbcc.js
@@ -315,10 +315,11 @@ function createGauge(){
             gauge2.Set('chart.title.top.bold','true');
             gauge2.Set('chart.title.bottom','Celsius');
             gauge2.Set('chart.title.bottom.size','8');
+            gauge2.value = 80;
             RGraph.Effects.Gauge.Grow(gauge2);
 
             gauge3 = new RGraph.Gauge("gauge_placeholder3", 1, 100, 0);
-            gauge3.Set('chart.title.top','Throttle Pos');
+            gauge3.Set('chart.title.top','Engine Load');
             gauge3.Set('chart.title.top.bold','true');
             gauge3.Set('chart.title.bottom','%');
             gauge3.Set('chart.title.bottom.size','8');
